c815e2066de0a45ada0186bb23f886ef7ea77bd2
Rahul Ram P <98167864+rahulram01@users.noreply.github.com>
2023-08-28 00:39:47 +0530

    function

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import reducer, { initialState } from "./reducer";
-import { StateProvider } from "./StateProvider";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <App />
-    </StateProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51Nj1yxSIuVLE1XhkdiIOvxIItn8C9iwKfyEsl9TFeQCmxweZ63Bh1Hbi7l3i9WvvMFO2a0iSb0PbqqdBdwefTw6u00Z3lJJGHy"
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.post("/payments/create", async (req, res) => {
+  try {
+    const total = req.query.total;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+      description: "Description of the product/service",
+      metadata: {
+        transaction_purpose_code: "P0101", // Example transaction purpose code
+      },
+    });
+
+    res.status(201).send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+exports.api = functions.https.onRequest(app);
